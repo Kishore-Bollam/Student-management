d585cc0032506ed3356b41aeedfe7f1340d9e20d
Kishore-Bollam <105742707+Kishore-Bollam@users.noreply.github.com>
2023-12-11 11:15:50 +0530

    count

--- src/practice/useState.js
+++ src/practice/useState.js
@@ -3,10 +3,18 @@ import React, { useState } from 'react'
 const UseState = () => {
 
     const [name , setName] = useState("kishore kumar bollam")
+    const [count , setCount] = useState(0)
   return (
     <div>
    <h1>{name}</h1>
    <button onClick={()=>setName("bollam")}>click me</button>
+
+
+   <h1>{count}</h1>
+   <button onClick={()=>setCount(count + 1)}>increment</button>
+   <button onClick={()=>setCount(count - 1)}>decrement</button>
+
+
     </div>
   )
 }
